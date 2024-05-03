631c225bcec12783321c54a2c28b365b2c04202a
Md.Moynull Hasan <moynull15-14141@diu.edu.bd>
2024-05-04 00:33:39 +0600

    new

--- lib/mood.dart
+++ lib/mood.dart
@@ -17,22 +17,91 @@ class mood extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: SizedBox(child: Image.asset('image/mood.jpg')),
                 ),
-                child: SizedBox(child: Image.asset('image/mood.jpg')),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Mood With Nature',
-                style: TextStyle(fontSize: 28),
-              )
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Mood With Nature',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  "In the gentle embrace of nature's bosom, moods sway like leaves in the wind, a delicate dance orchestrated by the rhythms of the earth. ",
+                  style: TextStyle(fontSize: 18),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'See More',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 12),
+                  ),
+                ),
+                Text(
+                  'Suggestions',
+                  style: TextStyle(color: Colors.green, fontSize: 18),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                        height: 200,
+                        width: 170,
+                        child: Stack(
+                          children: [
+                            Image.asset('image/city.jpg'),
+                            Positioned(
+                              top: 80,
+                              bottom: 20,
+                              right: 10,
+                              left: 130,
+                              child: Text(
+                                "City",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        )),
+                    Container(
+                        height: 200,
+                        width: 195,
+                        child: Stack(
+                          children: [
+                            Image.asset('image/road.jpg'),
+                            Positioned(
+                              top: 80,
+                              bottom: 20,
+                              right: 10,
+                              left: 130,
+                              child: Text(
+                                "Road",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
